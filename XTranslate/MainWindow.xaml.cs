8a6e7207744df285fe248821c04ea7da210a12f8
Phong Võ <kazer.vj@gmail.com>
2024-09-17 20:55:36 +0700

    .

--- XTranslate/MainWindow.xaml.cs
+++ XTranslate/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Forms;
 
 namespace XTranslate
 {
@@ -11,133 +13,61 @@ namespace XTranslate
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const int WM_HOTKEY = 0x0312;
-        private const int HOTKEY_ID = 1;
-        private const int MOD_CTRL = 0x0002;
-        private const int WH_MOUSE = 14;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
 
-        private IntPtr _hwnd;
-        private IntPtr _hook;
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            RegisterGlobalMouseHook();
-            RegisterGlobalHotkey();
-            _hwnd = new WindowInteropHelper(this).Handle;
+            _hookID = SetHook(_proc);
+            UnhookWindowsHookEx(_hookID);
         }
-
-        protected override void OnSourceInitialized(EventArgs e)
-
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
         {
-
-            base.OnSourceInitialized(e);
-            try
-
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
             {
-                HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
-                source.AddHook(new System.Windows.Interop.HwndSourceHook(WindowHook));
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
             }
+        }
 
-            catch (Exception ex)
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
 
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
-
-                // Handle the exception here
-
-                // You can log the error, display an error message to the user, etc.
-
-                Console.WriteLine("An EngineException occurred: " + ex.Message);
-
+                int vkCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine((Keys)vkCode);
             }
-
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
-        private IntPtr WindowHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-
-        {
-
-            return WndProc(hwnd, msg, wParam, lParam);
-
-        }
-        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam)
-
-        {
-
-            if (msg == WM_HOTKEY)
-            {
-                ActivateFunction();
-            }
-            return IntPtr.Zero;
-
-        }
-        public delegate IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled);
-
-        private void RegisterGlobalHotkey()
-        {
-            RegisterHotKey(_hwnd, HOTKEY_ID, MOD_CTRL, 0);
-        }
-
-        private void UnregisterGlobalHotkey()
-        {
-            UnregisterHotKey(_hwnd, HOTKEY_ID);
-        }
-
-        private void RegisterGlobalMouseHook()
-        {
-            _hook = SetWindowsHookEx(WH_MOUSE, MouseHookProc, IntPtr.Zero, 0);
-        }
-
-        private void UnregisterGlobalMouseHook()
-        {
-            UnhookWindowsHookEx(_hook);
-        }
-
-        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        private IntPtr MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode < 0) return CallNextHookEx(_hook, nCode, wParam, lParam);
-
-            if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Control) || Mouse.LeftButton != MouseButtonState.Pressed) return CallNextHookEx(_hook, nCode, wParam, lParam);
-
-            var selectedText = GetSelectedText();
-            if (string.IsNullOrEmpty(selectedText)) return CallNextHookEx(_hook, nCode, wParam, lParam);
-
-            ActivateFunction();
-            return CallNextHookEx(_hook, nCode, wParam, lParam);
-        }
-
-        private string GetSelectedText()
-        {
-            var textSelection = (TextSelection)Clipboard.GetData("System.Windows.Forms.Text");
-            return textSelection.Text;
-        }
-
-        private void ActivateFunction()
-        {
-            MessageBox.Show("Hotkey pressed!");
-        }
-
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            UnregisterGlobalHotkey();
-            UnregisterGlobalMouseHook();
+            
         }
 
-        [DllImport("user32")]
-        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-        [DllImport("user32")]
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-        [DllImport("user32")]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
 
-        [DllImport("user32")]
-        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
-
-        [DllImport("user32")]
-        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 
 }
