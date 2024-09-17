7e522f5b7c58f9a0529940380175ca227a3ad034
kzxl <65649029+kzxl@users.noreply.github.com>
2024-09-17 14:56:27 +0700

    update UI

--- XTranslate/App.xaml.cs
+++ XTranslate/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace XTranslate
 {
@@ -9,6 +10,27 @@ namespace XTranslate
     /// </summary>
     public partial class App : Application
     {
-    }
+        public App()
 
+        {
+
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+        }
+
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+
+        {
+
+            // Handle the exception here
+
+            // You can log the error, display an error message to the user, etc.
+
+            Console.WriteLine("An unhandled exception occurred: " + e.Exception.Message);
+
+            e.Handled = true;
+
+        }
+    }    
 }
